bb7fbea9280bc2eb5a26338dc5e8c6d0d75399fe
EdmundLimKokHeng <48701576+EdmundLimKokHeng@users.noreply.github.com>
2019-05-30 07:24:06 +0800

    Add files via upload

--- Hotel.sol
+++ Hotel.sol
@@ -3,6 +3,12 @@
 pragma solidity >=0.4.25 <0.6.0;
 
 contract Hotel_Proposal1 {
+
+    struct Entry{
+        uint index; // index start 1 to keyList.length
+        bytes32 value;
+    }
+
     int storedData; //state variables
 	string storedSecretData;
 	string userIDAccessed;
@@ -10,39 +16,67 @@ contract Hotel_Proposal1 {
 	string accessedTimestamp;
 	string currentSecret;
 	string Unsealed;
-	int Hotel_SGD_Earnings = 0;
-	string Hotel_ETH_address = "0xfdd7aa0b1bcbb77df50b9ef2a5559808d07396d7";
+	uint Hotel_SGD_Earnings = 0;
+	uint Hotel_SGD_Holdings = 0;
+	string Hotel_ETH_address = '0xfdd7aa0b1bcbb77df50b9ef2a5559808d07396d7';
 	
 	
     string ManagerName;
-	int RequiredDepositName;
+	uint RequiredDepositName;
 	string CurrencyDenomination;
-	int PackagePrice;
+	uint PackagePrice;
 	int NumofDays;
 	string NumAndTypeOfRooms;
-	int EarlyBirdDiscount;
+	uint EarlyBirdDiscount;
 	uint EarlyBirdEndDate;
+
 	
 	
-string[] CustomerListDeposit;
-string[] CustomerListDenomination;
-mapping (string => string) customerDeposit; //first value is address, 2nd value is the deposited amount
+//string[] CustomerListDeposit;
+//string[] CustomerListDenomination;
+mapping (string => uint) customerDeposit; //first value is address, 2nd value is the deposited amount
 mapping (string => string) customerDenomination; // tocheck the denomination that the customer used for payment, first value is customer address, 2nd value is denomination
 
 
 
-function addCustomerDeposit2List(string memory customerAcct,string memory deposit) public{
+function addCustomerDeposit2List(string memory customerAcct,uint deposit) public{
   customerDeposit[customerAcct] =  deposit;
-  CustomerListDeposit.push(customerAcct);
+  //CustomerListDeposit.push(customerAcct);
 }
 
 function addCustomerDenomination2List(string memory customerAcct,string memory denomination) public{
   customerDenomination[customerAcct] =  denomination;
-  CustomerListDenomination.push(customerAcct);
+  //CustomerListDenomination.push(customerAcct);
 }
 
+    function remove(string memory customerAcct) public {
+        //Entry storage entry = customerDeposit[customerAcct];
+        //require(entry.index != 0); // entry not exist
+        //require(entry.index <= CustomerListDeposit.length); // invalid index value
+        
+        // Move an last element of array into the vacated key slot.
+        //uint keyListIndex = entry.index - 1;
+        //uint keyListLastIndex = CustomerListDeposit.length - 1;
+        //customerDeposit[CustomerListDeposit[keyListLastIndex]].index = keyListIndex + 1;
+        //CustomerListDeposit[keyListIndex] = CustomerListDeposit[keyListLastIndex];
+        //CustomerListDeposit.length--;
+        delete customerDeposit[customerAcct];
+		
+		//--------------------------------------
+        //Entry storage entry2 = customerDenomination[customerAcct];
+        //require(entry2.index != 0); // entry not exist
+        //require(entry2.index <= CustomerListDenomination.length); // invalid index value
+        
+        // Move an last element of array into the vacated key slot.
+        //uint keyListIndex2 = entry2.index - 1;
+        //uint keyListLastIndex2 = CustomerListDenomination.length - 1;
+        //customerDenomination[CustomerListDenomination[keyListLastIndex2]].index = keyListIndex2 + 1;
+        //CustomerListDenomination[keyListIndex2] = CustomerListDenomination[keyListLastIndex2];
+        //CustomerListDenomination.length--;
+        delete customerDenomination[customerAcct];
+    }
 
-    function getDepositByKey(string memory customerAcct) public view returns (string memory) {
+    function getDepositByKey(string memory customerAcct) public view returns (uint) {
         return customerDeposit[customerAcct];
     }
 
@@ -50,14 +84,49 @@ function addCustomerDenomination2List(string memory customerAcct,string memory d
         return customerDenomination[customerAcct];
     }
 	
+	
+	function customerCheckOutSGD(string memory customerAcct,string memory setHotelMessageMode) public payable {
+		string memory paidDenomination = getDenominationByKey(customerAcct);
+		uint paidAmount = getDepositByKey(customerAcct);
+		
 
+		  Hotel_SGD_Earnings = Hotel_SGD_Earnings + paidAmount + 60;
+		  Hotel_SGD_Holdings = Hotel_SGD_Holdings - paidAmount - 60;
+
+		remove(customerAcct);
+	}
+	
+	
+	function customerCheckOutETH(string memory customerAcct,address payable hotelAdd, string memory setHotelMessageMode) public payable {
+		string memory paidDenomination = getDenominationByKey(customerAcct);
+		uint paidAmount = getDepositByKey(customerAcct);
+		
+         hotelAdd.transfer(paidAmount);
+
+
+		remove(customerAcct);
+	}
 	
 
 
+    function deposit(uint256 amount) payable public {
+        require(msg.value == amount);
+        // nothing else to do!
+    }
 
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+	
+    function getSGDBalance() public view returns (uint) {
+        return Hotel_SGD_Holdings;
+    }
 
+    function getSGDEarnings() public view returns (uint) {
+        return Hotel_SGD_Earnings;
+    }
 
-    function set(string memory setManagerName,int setRequiredDepositName,string memory setCurrencyDenomination,int setPackagePrice,int setNumofDays,string memory setNumAndTypeOfRooms,int setEarlyBirdDiscount, uint setEarlyBirdEndDate, string memory setHotelMessageMode) public payable {
+    function set(string memory setManagerName,uint setRequiredDepositName,string memory setCurrencyDenomination,uint setPackagePrice,int setNumofDays,string memory setNumAndTypeOfRooms,uint setEarlyBirdDiscount, uint setEarlyBirdEndDate, string memory setHotelMessageMode) public payable {
         ManagerName = setManagerName;
 	    RequiredDepositName = setRequiredDepositName;
 	    CurrencyDenomination = setCurrencyDenomination;
@@ -69,16 +138,20 @@ function addCustomerDenomination2List(string memory customerAcct,string memory d
     }
 	
 	
-    function paymentHolding(string memory customerAcct, string memory paymentDenomination,string memory paymentAmount, string memory setHotelMessageMode) public payable {
+    function paymentHolding(string memory customerAcct, string memory paymentDenomination,uint paymentAmount, string memory setHotelMessageMode) public payable {
         addCustomerDeposit2List(customerAcct,paymentAmount);
 		addCustomerDenomination2List(customerAcct,paymentDenomination);
+		if(compareStrings(paymentDenomination,"SGD"))
+		{
+			Hotel_SGD_Holdings = Hotel_SGD_Holdings + paymentAmount;
+		}
     }
 
     function getManagerName() public view returns (string memory) {
         return ManagerName;	
     }
 	
-    function getRequiredDepositName() public view returns (int) {
+    function getRequiredDepositName() public view returns (uint) {
         return RequiredDepositName;	
     }
 	
@@ -86,7 +159,7 @@ function addCustomerDenomination2List(string memory customerAcct,string memory d
         return CurrencyDenomination;	
     }
 	
-    function getPackagePrice() public view returns (int) {
+    function getPackagePrice() public view returns (uint) {
         return PackagePrice;	
     }
 	
@@ -98,7 +171,7 @@ function addCustomerDenomination2List(string memory customerAcct,string memory d
         return NumAndTypeOfRooms;	
     }
 	
-    function getEarlyBirdDiscount() public view returns (int) {
+    function getEarlyBirdDiscount() public view returns (uint) {
 		if (EarlyBirdEndDate > now)
 		{
 			return 0;
